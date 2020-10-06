748603b54e69da298f55b06e47f53e81469899de
Toglefritz <contact@hatfieldscott.com>
2020-10-05 22:22:32 -0500

    Added code frontmatter.

---
+++ Code/The_Sketch_of_Knot/The_Sketch_of_Knot.ino
@@ -0,0 +1,37 @@
+/*
+    Project: Knot, a robotic offcut piece of scrap lumber
+
+  __/\\\________/\\\___________________________________________        
+   _\/\\\_____/\\\//____________________________________________       
+    _\/\\\__/\\\//_____________________________________/\\\______      
+     _\/\\\\\\//\\\______/\\/\\\\\\_______/\\\\\_____/\\\\\\\\\\\_     
+      _\/\\\//_\//\\\____\/\\\////\\\____/\\\///\\\__\////\\\////__    
+       _\/\\\____\//\\\___\/\\\__\//\\\__/\\\__\//\\\____\/\\\______   
+        _\/\\\_____\//\\\__\/\\\___\/\\\_\//\\\__/\\\_____\/\\\_/\\__  
+         _\/\\\______\//\\\_\/\\\___\/\\\__\///\\\\\/______\//\\\\\___ 
+          _\///________\///__\///____\///_____\/////_________\/////____
+      
+    Author: Toglefritz
+
+    Description: Knot represents what is arguably the final evolutionary form of a spare
+    bit of 2x4 lumber. By hollowing out the wood, it can be filled by a RedBoard Artemis,
+    the board for which this sketch was written, two stepper motor controllers, and two
+    stepper motors. The whole thing is designs such that all this tech is hidden when
+    the robot is viewed from the top, meaning it just looks like a normal piece of non-robotic
+    wood when viewed from that perspective. The robot is controlled by tapping on the top
+    of the wooden chassis. One tap moves the robot forward, two taps turns right, three 
+    turns left, and four moves the robot backward.
+
+    Instructions for building the robot can be found on Instructables.
+*/
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
